4d4a5bf2fdda2982088e8685c9e5b37f3522f0bb
pinochio <211191350+har-012@users.noreply.github.com>
2026-03-30 11:57:33 -0400

    Still refinement needed for conditional logic

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    if(num < 0){
+        return 'ERROR';
+    }
+    if(str === ''){
+        return '';
+    }
+    else if(num === 0){
+        return ''
+    }
+    let newStr = str;
+    for(let i = 1;i < num;i++){
+        newStr += str;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
