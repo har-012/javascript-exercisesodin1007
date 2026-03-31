6bf7ea8907d93d318c170ff0565a4512680bc599
pinochio <211191350+har-012@users.noreply.github.com>
2026-03-31 14:49:22 -0400

    Still need to get clarity on array method, but go- -ing good on logic

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newStr = [];
+    for(let i = 0;i < str.length;i++){
+        newStr[i] = str[i];
+    }
+    return newStr.reverse().join("");
 };
 
 // Do not edit below this line
