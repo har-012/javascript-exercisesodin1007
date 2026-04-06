2bcae29a9d3636e1d2d3eda3f0bc000f9590b88e
pinochio <211191350+har-012@users.noreply.github.com>
2026-04-06 12:07:44 -0400

    still need to practice on array methods

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...theArgs) {
+    for(let arg of theArgs){
+        if(arr.includes(arg)){
+            let i = arr.indexOf(arg);
+            arr.splice(i,1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
