652819f1f9f9b58d5c0fd674549052cffc3f8bfe
pinochio <211191350+har-012@users.noreply.github.com>
2026-04-13 11:15:22 -0400

    Still need to get grasp on this language

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
 
+    if(Number.isInteger(num1) && Number.isInteger(num2)){
+        if(num1<0 || num2<0) return "ERROR";
+        let small,large,sum = 0 ;
+        if(num1 > num2){ 
+            small = num2;
+            large = num1;
+        }
+        else{
+            small = num1;
+            large = num2;
+        }
+        for(let i = small;i <= large;i++){
+            sum += i;
+        }
+        return sum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
